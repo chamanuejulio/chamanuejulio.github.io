9889742863ad4de4303d2d18f43c6cc3651f1ac7
Julio César Mello Boaroli <julio.boaroli@mercadolivre.com>
2024-01-16 22:45:04 -0300

    feat: release

--- src/components/GiftList.tsx
+++ src/components/GiftList.tsx
@@ -5,7 +5,6 @@ interface Gift {
   id: number;
   username: string;
   imageUrl: string;
-  requires: boolean,
   caption: string;
   value: number;
   pixUrl: string;
@@ -24,7 +23,7 @@ const GiftList: React.FC<GiftListProps> = ({ gifts }) => {
       {gifts.map((gift) => (
         <div key={gift.id} className="gift-post">
           <div className='contenedor-imagem'>
-            {gift.requires ? <img src={require('../assets/julin.png')} alt={`Post by ${gift.username}`} /> : <img src={gift.imageUrl} alt={`Post by ${gift.username}`} />}
+            <img src={gift.imageUrl} alt={`Post by ${gift.username}`} />
           </div>
           <div className="caption">{gift.caption}</div>
           <div className="username">{gift.username}</div>
@@ -32,6 +31,42 @@ const GiftList: React.FC<GiftListProps> = ({ gifts }) => {
           <button className="pix" onClick={() => handlePixClick(gift.pixUrl)}>Presentear</button>
         </div>
       ))}
+      {/* <div key={gifts[18].id} className="gift-post">
+        <div className='contenedor-imagem'>
+          <img src={require('../assets/julin.png')} alt={`Post by ${gifts[18].username}`} />
+        </div>
+        <div className="caption">{gifts[18].caption}</div>
+        <div className="username">{gifts[18].username}</div>
+        <div className='value'>{formatNumberToReal(gifts[18].value)}</div>
+        <button className="pix" onClick={() => handlePixClick(gifts[18].pixUrl)}>Presentear</button>
+      </div> */}
+      {/* <div key={gifts[21].id} className="gift-post">
+        <div className='contenedor-imagem'>
+          <img src={require('../assets/cross.png')} alt={`Post by ${gifts[21].username}`} />
+        </div>
+        <div className="caption">{gifts[21].caption}</div>
+        <div className="username">{gifts[21].username}</div>
+        <div className='value'>{formatNumberToReal(gifts[21].value)}</div>
+        <button className="pix" onClick={() => handlePixClick(gifts[21].pixUrl)}>Presentear</button>
+      </div>
+      <div key={gifts[22].id} className="gift-post">
+        <div className='contenedor-imagem'>
+          <img src={require('../assets/blogueira.png')} alt={`Post by ${gifts[22].username}`} />
+        </div>
+        <div className="caption">{gifts[22].caption}</div>
+        <div className="username">{gifts[22].username}</div>
+        <div className='value'>{formatNumberToReal(gifts[22].value)}</div>
+        <button className="pix" onClick={() => handlePixClick(gifts[22].pixUrl)}>Presentear</button>
+      </div>
+      <div key={gifts[24].id} className="gift-post">
+        <div className='contenedor-imagem'>
+          <img src={gifts[24].imageUrl} alt={`Post by ${gifts[24].username}`} />
+        </div>
+        <div className="caption">{gifts[24].caption}</div>
+        <div className="username">{gifts[24].username}</div>
+        <div className='value'>{formatNumberToReal(gifts[24].value)}</div>
+        <button className="pix" onClick={() => handlePixClick(gifts[24].pixUrl)}>Presentear</button>
+      </div> */}
     </div>
   );
 };
--- src/views/pages/BridalShower.tsx
+++ src/views/pages/BridalShower.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import  GiftList from '../../components/GiftList'
+import GiftList from '../../components/GiftList'
 import gifts from '../../store/BridalShowerGifts.json'
+
+import './BridalShower.css';
 
 function BridalShower() {
   return (
-    <div className="BridalShower">
-      <GiftList gifts={gifts}/>
+    <div className="bridal-shower">
+      <GiftList gifts={gifts} />
     </div>
   );
 }
